f5fd06f7fcf8ed0056472a45be9e997f912694c3
Raffael Silva <raffaeleloi121@gmail.com>
2022-07-21 19:27:03 -0300

    addjust the validation of update customer

--- app/Http/Requests/Customer/CustomerUpdateRequest.php
+++ app/Http/Requests/Customer/CustomerUpdateRequest.php
@@ -25,7 +25,14 @@ class CustomerUpdateRequest extends FormRequest
     {
         return [
             'name'            => 'string|min:4|max:255',
+            'document'        => 'string|nullable', 
             'phone'           => 'string',
+            'address'         => 'string|nullable',  
+            'city'            => 'string|nullable', 
+            'state'           => 'string|nullable', 
+            'email'           => 'string|nullable',
+            'social_contract' => 'string|nullable',
+            'contact'         => 'string|nullable',
         ];
     }
 
@@ -35,8 +42,14 @@ class CustomerUpdateRequest extends FormRequest
             'name.string'            => 'O campo nome deve ser do tipo texto', 
             'name.min'               => 'O campo nome deve ter no mínimo 4 caracteres', 
             'name.max'               => 'O campo nome deve ter no máximo 255 caracteres',
+            'document.string'        => 'O campo documento deve ser do tipo texto',
             'phone.string'           => 'O campo telefone deve ser do tipo texto',
             'address.string'         => 'O campo endereço deve ser do tipo texto',
+            'city.string'            => 'O campo cidade deve ser do tipo texto',
+            'state.string'           => 'O campo estado deve ser do tipo texto',
+            'email.string'           => 'O campo email deve ser do tipo texto',
+            'social_contract.string' => 'O campo contrato social deve ser do tipo texto',
+            'contact.string'         => 'O campo contato deve ser do tipo texto',
         ];
     }
 }
