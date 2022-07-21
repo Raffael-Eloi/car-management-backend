cb76a7df24086f3cafc568e06c0f77ed1d64c446
Raffael Silva <raffaeleloi121@gmail.com>
2022-07-21 19:42:32 -0300

    addjust the validation of store and update on user

--- app/Http/Requests/User/UserUpdateRequest.php
+++ app/Http/Requests/User/UserUpdateRequest.php
@@ -24,22 +24,26 @@ class UserUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'     => 'string|min:4|max:255',
+            'name'     => 'string|min:4|max:255', 
+            'document' => 'string', 
+            'phone'    => 'string',
+            'address'  => 'string',  
             'city'     => 'string', 
             'state'    => 'string', 
-            'email'    => 'string',
+            'email'    => 'string', 
         ];
     }
 
     public function messages()
     {
         return [
-            'name.string'       => 'O campo nome deve ser do tipo texto', 
-            'name.min'          => 'O campo nome deve ter no mínimo 4 caracteres', 
-            'name.max'          => 'O campo nome deve ter no máximo 255 caracteres', 
-            'city.string'       => 'O campo cidade deve ser do tipo texto', 
-            'state.string'      => 'O campo estado deve ser do tipo texto', 
-            'email.string'      => 'O campo email deve ser do tipo texto', 
+            'name.string'    => 'O campo nome deve ser do tipo texto', 
+            'name.min'       => 'O campo nome deve ter no mínimo 4 caracteres', 
+            'name.max'       => 'O campo nome deve ter no máximo 255 caracteres', 
+            'address.string' => 'O campo endereço deve ser do tipo texto',
+            'city.string'    => 'O campo cidade deve ser do tipo texto',
+            'state.string'   => 'O campo estado deve ser do tipo texto',
+            'email.string'   => 'O campo email deve ser do tipo texto',
         ];
     }
 }
